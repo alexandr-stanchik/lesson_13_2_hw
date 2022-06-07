3b400e5c1350a2aac13a64248393ca78a4b0f2f0
alexandr-stanchik <lex023ott@mail.ru>
2022-06-08 02:10:58 +0300

    Uncommented code

--- lesson_13_2_hw.py
+++ lesson_13_2_hw.py
@@ -2,14 +2,14 @@
 # Напишите функцию, которая создает список случайных
 # элементов. На фход функция принимает кол-во элементов,
 # минимальное и максимальное значение
-# from random import randint
-#
-#
-# def rand_nums(count, min_item, max_item):
-#     return [randint(min_item, max_item) for i in range(count)]
-#
-#
-# print(rand_nums(int(input('count elements: ')), int(input('min value: ')), int(input('max value: '))))
+from random import randint
+
+
+def rand_nums(count, min_item, max_item):
+    return [randint(min_item, max_item) for i in range(count)]
+
+
+print(rand_nums(int(input('count elements: ')), int(input('min value: ')), int(input('max value: '))))
 
 
 # exam_2_5
@@ -30,40 +30,39 @@
 # print(f'К оплате {cakes[cake][1] * weight / 100}')
 # print(f'торта {cake} осталось {cakes[cake][-1] - weight} грамм')
 
-# def answer(cake, descr):
-#     if descr == 'описание':
-#         print(f'Торт {cake} состоит из {cakes[cake][0]}')
-#     elif descr == 'цена':
-#         print(f'Торт {cake} стоит {cakes[cake][1]} рублей')
-#     elif descr == 'количество':
-#         print(f'Торт {cake} осталось {cakes[cake][2]} грамм')
-#
-#
-# cakes = {'наполеон': [['масло', 'мука', 'сахар'], 50, 1000],
-#          'медовик': [['масло', 'мука', 'сметанный крем'], 100, 1000],
-#          'киевский': [['масло', 'мука', 'шоколадный крем'], 150, 1000]}
-# cake = input('Какой торт вы бы хотели приобрести? ').lower()
-# descr = input('Что бы вы хотели уточнить? ').lower()
-#
-# answer(cake, descr)
-#
-# weight = int(input('Сколько торта Вам положить? '))
-# print(f'К оплате {cakes[cake][1] * weight / 100}')
-# print(f'торта {cake} осталось {cakes[cake][-1] - weight} грамм')
+def answer(cake_1, descr_1):
+    if descr_1 == 'описание':
+        print(f'Торт {cake_1} состоит из {cakes[cake_1][0]}')
+    elif descr_1 == 'цена':
+        print(f'Торт {cake_1} стоит {cakes[cake_1][1]} рублей')
+    elif descr_1 == 'количество':
+        print(f'Торт {cake_1} осталось {cakes[cake_1][2]} грамм')
+
+
+cakes = {'наполеон': [['масло', 'мука', 'сахар'], 50, 1000],
+         'медовик': [['масло', 'мука', 'сметанный крем'], 100, 1000],
+         'киевский': [['масло', 'мука', 'шоколадный крем'], 150, 1000]}
+cake = input('Какой торт вы бы хотели приобрести? ').lower()
+descr = input('Что бы вы хотели уточнить? ').lower()
+
+answer(cake, descr)
+
+weight = int(input('Сколько торта Вам положить? '))
+print(f'К оплате {cakes[cake][1] * weight / 100}')
+print(f'торта {cake} осталось {cakes[cake][-1] - weight} грамм')
 
 
 # Hometask_13_6
 # Напишите функцию, вычисляющую значение
 # факториала числа N. Используйте рекурсию
+def fact(x):
+    if x == 1:
+        return 1
+    return fact(x - 1) * x
 
-#
-# def fact(x):
-#     if x == 1:
-#         return 1
-#     return fact(x - 1) * x
-#
-#
-# print(fact(int(input())))
+
+print(fact(int(input())))
+
 
 # 13.11
 # Напишите декоратор к функции деления, который меняет
